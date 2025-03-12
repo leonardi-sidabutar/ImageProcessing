0fab77154283d473c8d8e9d0cfd0b0cd5a585a1c
Leonardi Sidabutar <sidabutarl@socfindo.co.id>
2025-03-12 10:38:22 +0700

    update

--- citra/segmentasi_morfologi_hsi.py
+++ citra/segmentasi_morfologi_hsi.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # === 1. BACA CITRA ===
-img = cv2.imread("tomat/matang/1.jpg")
+img = cv2.imread("../program/tomat/matang/1.jpg")
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Konversi ke RGB untuk ditampilkan di matplotlib
 
 # === 2. KONVERSI KE GRAYSCALE ===
